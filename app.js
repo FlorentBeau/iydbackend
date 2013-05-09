863b82f39ac5ec4cf40884eb3f910e301e2c191f
Michael Sokol <mikaa123@gmail.com>
2013-05-09 18:20:19 +0200

    removing node_modules

--- app.js
+++ app.js
@@ -5,23 +5,25 @@ var server = app.listen(process.env.PORT, function() {
   console.log('Listening on', process.env.PORT);
 });
 
-app.get('/posts', function(req, res) {
+app.get('/dreams', function(req, res) {
   res.set({ 'Content-Type': 'application/json' });
-  var result = [{
-    id: 1,
-    dream: 'Staive dream',
-    pseudo: 'Cousin',
-    sex: 'Male'
-  }, {
-    id: 2,
-    dream: 'yo mama',
-    pseudo: 'machin',
-    sex: 'Male'
-  }, {
-    id: 3,
-    dream: 'waiiiishe',
-    pseudo: 'georges',
-    sex: 'Female'
-  }];
+  var result = {
+    dreams: [{
+      id: 1,
+      dream: 'Staive dream',
+      pseudo: 'Cousin',
+      sex: 'Male'
+    }, {
+      id: 2,
+      dream: 'yo mama',
+      pseudo: 'machin',
+      sex: 'Male'
+    }, {
+      id: 3,
+      dream: 'waiiiishe',
+      pseudo: 'georges',
+      sex: 'Female'
+    }]
+  };
   res.send(JSON.stringify(result));
 });
